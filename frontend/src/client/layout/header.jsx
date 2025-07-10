a7039c2d3ccd28d137158db26fa199eeba4ed173
Adnna Haider <adnanhaider9993@gmail.com>
2025-07-10 12:36:34 +0500

    Revert "fix layout"
    This reverts commit ff4404935d397a05276277da8fe45ed25d034b86.

--- frontend/src/client/layout/header.jsx
+++ frontend/src/client/layout/header.jsx
@@ -46,12 +46,12 @@ const Header = ({ onHamburgerClick }) => {
       {/* Right side - User actions */}
       <div className="flex items-center gap-3">
         {/* Profile Icon */}
-        <button className="p-3  hover:bg-gray-700  transition-colors border border-gray-400 cursor-pointer">
+        <button className="p-3 rounded-lg hover:bg-gray-700  transition-colors border border-gray-400 cursor-pointer">
           <User className="text-gray-300 w-3 h-3 lg:w-5 lg:h-5" />
         </button>
 
         {/* Notification Bell with badge */}
-        <button className="p-3  transition-colors relative border border-gray-400 hover:bg-gray-700 cursor-pointer">
+        <button className="p-3 rounded-lg  = transition-colors relative border border-gray-400 hover:bg-gray-700 cursor-pointer">
           <Bell className="text-gray-300 w-3 h-3 lg:w-5 lg:h-5" />
           <span className="absolute -top-1 -right-1 bg-red-500 text-white text-xs rounded-full w-5 h-5 flex items-center justify-center ">
             1
@@ -59,7 +59,7 @@ const Header = ({ onHamburgerClick }) => {
         </button>
 
         {/* Add Admin Button */}
-        <button className="p-3  hover:bg-gray-700 text-white text-sm font-medium  border border-gray-400 transition-colors cursor-pointer">
+        <button className="p-3  hover:bg-gray-700 text-white text-sm font-medium rounded-lg border border-gray-400 transition-colors cursor-pointer">
           Add Admin
         </button>
       </div>
