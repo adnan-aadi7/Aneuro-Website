504d21ec0f41b5d58762fc32d41398a2bde78ddd
kainat robi <kainatrobi01@gmail.com>
2025-08-06 15:40:29 +0500

    changes

--- backend/index.js
+++ backend/index.js
@@ -13,7 +13,7 @@ import webhookRoutes from './routes/webhookRoutes.js';
 import googlePassport from './services/googlePassport.js';
 import facebookPassport from './services/facebookPassport.js';
 import emailSequenceRoutes from './routes/emailSequencRoute.js';
-
+import promptpackRoutes from './routes/promptPackRoutes.js'
 const app = express();
 const port = 3000;
 
@@ -60,6 +60,7 @@ const startServer = async () => {
     app.use("/api/ticket", ticketRoutes);
     app.use("/api/payment", paymentRoutes);
     app.use("/api/email-sequences", emailSequenceRoutes);
+    app.use("/api/prompt-packs", promptpackRoutes);
 
     // Start server only after database connection is established
     app.listen(port, () => {
