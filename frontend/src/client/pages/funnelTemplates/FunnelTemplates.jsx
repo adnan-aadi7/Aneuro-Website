df94b76d9cf092a53da8b1c78f5840b270283304
Aafhan Javed <101995993+afanjaved@users.noreply.github.com>
2025-08-20 17:23:46 +0500

    done with funnel templates

--- frontend/src/client/pages/funnelTemplates/FunnelTemplates.jsx
+++ frontend/src/client/pages/funnelTemplates/FunnelTemplates.jsx
@@ -18,9 +18,6 @@ const FunnelTemplates = () => {
 
   useEffect(() => {
     dispatch(fetchFunnelTemplates({ brainType: activeTab, category }));
-    console.log('====================================');
-    console.log(templates);
-    console.log('====================================');
   }, [dispatch, activeTab, category]);
 
   // client-side fallback
