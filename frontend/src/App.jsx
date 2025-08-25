d931cc1d12cb682dbee801c26bf1d810394a5b6f
kainat robi <kainatrobi01@gmail.com>
2025-08-25 10:19:06 +0500

    changes

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -13,9 +13,8 @@ import GoogleCallback from "./auth/components/login/GoogleCallback";
 import FacebookCallback from "./auth/components/login/FacebookCallback";
 
 // guards
-import RequireAuth from "./components/auth/RequireAuth";
-import RequireRole from "./components/auth/RequireRole";
-
+import RequireAuth from "./auth/RequireAuth";
+import RequireRole from "./auth/components/RequireRole";
 // client
 import Dashboard from "./client/pages/dashboard/Dashboard";
 import Subcription from "./auth/pages/choosingPlan/Subcription";
